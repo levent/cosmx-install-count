3f18f44a014f3a5d442274db486b068e59256782
Levent Ali <lebreeze@gmail.com>
2012-06-29 10:49:07 +0100

    API KEY check

--- cosmx_count.rb
+++ cosmx_count.rb
@@ -1,6 +1,7 @@
 require 'json'
 class CosmxCount < Sinatra::Base
   get '/' do
+    raise "No API KEY" if ENV['API_KEY'].nil?
     response = HTTParty.get("http://api.cosm.com/v2/feeds.json?tag=app%3Aname%3DCosmX&content=summary&key=#{ENV['API_KEY']}")
     json = JSON.parse(response.body)
     json['totalResults'].to_s
