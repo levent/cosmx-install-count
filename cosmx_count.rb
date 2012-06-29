698d5957f9dbdf7e8216fbd0ef41a5ecddbe1b53
Levent Ali <lebreeze@gmail.com>
2012-06-29 11:07:31 +0100

    Pass on user agent

--- cosmx_count.rb
+++ cosmx_count.rb
@@ -2,7 +2,7 @@ require 'json'
 class CosmxCount < Sinatra::Base
   get '/' do
     raise "No API KEY" if ENV['API_KEY'].nil?
-    response = HTTParty.get("http://api.cosm.com/v2/feeds.json?tag=app%3Aname%3DCosmX&content=summary&key=#{ENV['API_KEY']}")
+    response = HTTParty.get("http://api.cosm.com/v2/feeds.json?tag=app%3Aname%3DCosmX&content=summary&key=#{ENV['API_KEY']}", :headers => {"User-Agent" => request.user_agent})
     json = JSON.parse(response.body)
     json['totalResults'].to_s
   end
